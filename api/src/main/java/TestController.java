009216a58c00eb33d6ef6a105c1e660a42300355
horobito <odkfu1@gmail.com>
2022-04-05 22:52:18 +0900

    feat : set [api] module

---
+++ api/src/main/java/TestController.java
@@ -0,0 +1,9 @@
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+
+    private final TestService testService;
+}
