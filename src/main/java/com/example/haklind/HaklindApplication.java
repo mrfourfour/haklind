65aeca1591ac11df903f8ff5febbb40d8bfd000f
horobito <odkfu1@gmail.com>
2022-04-05 22:24:51 +0900

    feat : set init

---
+++ src/main/java/com/example/haklind/HaklindApplication.java
@@ -0,0 +1,13 @@
+package com.example.haklind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaklindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaklindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/haklind/HaklindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.haklind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaklindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
